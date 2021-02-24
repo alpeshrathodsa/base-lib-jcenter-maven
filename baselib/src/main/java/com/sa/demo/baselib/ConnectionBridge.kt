574a51e29265136f9f15cfcd5c85dcf668772097
alpeshrathodsa <alpesh.rathod.sa@gmail.com>
2021-02-24 17:11:47 +0530

    Inital Setup

---
+++ baselib/src/main/java/com/sa/demo/baselib/ConnectionBridge.kt
@@ -0,0 +1,13 @@
+package com.sa.demo.baselib
+
+/**
+ * Purpose -
+ *
+ * @author Alpesh Rathod
+ *
+ * Created on 2/1/21
+ */
+interface ConnectionBridge {
+
+    fun isNetworkAvailable(): Boolean
+}
